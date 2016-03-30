713fc3aac5b252439db229c6a185513629699ec3
Bertalan Verebelyi <bertalan.verebelyi@gmail.com>
2016-03-30 21:40:09 +0200

    Auto stash for revert of "basic version"

---
+++ src/ResponseHelper.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace LaravelApiResponse;
+
+use Illuminate\Http\Response as IlluminateResponse;
+use Illuminate\Support\Facades\Response;
+
+class ResponseHelper
+{
+    /**
+     * @var int
+     */
+    protected $statusCode = IlluminateResponse::HTTP_OK;
+
+    /**
+     * @param $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return  $this->statusCode;
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function respondNotFound($message = 'Not Found!')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_NOT_FOUND)->respondWithError($message);
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function respondInternalError($message = 'Internal Error!')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_INTERNAL_SERVER_ERROR)->respondWithError($message);
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function respondInvalidParameters($message = 'Invalid Parameters!')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_UNPROCESSABLE_ENTITY)->respondWithError($message);
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function respondCreated($message = 'Resource Successfully Created.')
+    {
+        return $this->setStatusCode(IlluminateResponse::HTTP_CREATED)->respond([
+            'message' => $message
+        ]);
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @return mixed
+     */
+    public function respond($data, $headers = [])
+    {
+        return Response::json($data, $this->getStatusCode(), $headers);
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    protected function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
+}
